f58025d16e4a28eb94e5b3e956e791e2b990fa16
Adeyemo Adekunle <adekunle@nestcoin.com>
2023-05-11 06:56:35 +0100

    feat : screen util

---
+++ lib/app.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:market_place_assignment/gen/fonts.gen.dart';
+import 'package:market_place_assignment/src/core/app_colors.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Market Place',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: TextTheme(
+              headline1: TextStyle(
+                fontSize: 24.sp,
+                color: AppColors.headlineTextColor,
+                fontWeight: FontWeight.w700,
+                fontFamily: FontFamily.openSans,
+              ),
+              bodyText1: TextStyle(
+                fontSize: 12.sp,
+                color: AppColors.bodyTextColor,
+                fontWeight: FontWeight.w400,
+                fontFamily: FontFamily.openSans,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
